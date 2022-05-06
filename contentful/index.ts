d37fabcd56710362b84d4ec578140dda577141fc
nijat ibrahimov <ibrahimovofficial@gmail.com>
2022-05-06 19:21:02 +0400

    start projects

---
+++ contentful/index.ts
@@ -0,0 +1,11 @@
+import { createClient } from 'contentful'
+
+
+const client = createClient({
+	
+	space: 'zsvyg91ffw31',
+	accessToken: '0FZDj693wpfAO5Kfr3cjL2c_hQW7aqKW44z6aqYn63I'
+	
+})
+
+export default client
